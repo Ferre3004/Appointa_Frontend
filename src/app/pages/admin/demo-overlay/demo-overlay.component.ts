07ac33230f78fcb352c8ce7b0b5032958b50a958
aferreyra <aferreyra@interfile.com.ar>
2026-04-07 22:38:21 -0300

    import common module in demo overlay

--- src/app/pages/admin/demo-overlay/demo-overlay.component.ts
+++ src/app/pages/admin/demo-overlay/demo-overlay.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-demo-overlay',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './demo-overlay.component.html',
   styleUrls: ['./demo-overlay.component.scss']
 })
